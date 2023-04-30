1c9e9963d2a09fa65afd9fe0333a577afdefe1d4
yatharthw <121886758+SaxenaVaibhav80@users.noreply.github.com>
2023-05-01 00:14:25 +0530

    change1

---
+++ public/js/pro.js
@@ -0,0 +1,67 @@
+const a=document.getElementById("fitt")
+a.addEventListener("mouseover",function()
+ {
+    document.getElementsByClassName("fitness-div")[0].style.visibility="visible";
+})
+const b=document.getElementsByClassName("fitness-div")[0];
+b.addEventListener("mouseover",function()
+{
+    document.getElementsByClassName("fitness-div")[0].style.visibility="visible";
+})
+b.addEventListener("mouseout",function()
+{
+    document.getElementsByClassName("fitness-div")[0].style.visibility="hidden";
+})
+
+const c=document.getElementById("boook")
+console.log(a)
+c.addEventListener("mouseover",function()
+ {
+    document.getElementsByClassName("book-div")[0].style.visibility="visible";
+})
+const d=document.getElementsByClassName("book-div")[0];
+d.addEventListener("mouseover",function()
+{
+    document.getElementsByClassName("book-div")[0].style.visibility="visible";
+})
+d.addEventListener("mouseout",function()
+{
+    document.getElementsByClassName("book-div")[0].style.visibility="hidden";
+})
+
+$(".signup-box").hide();
+
+$(".signup-rout").click(function()
+{   $(".signup-rout").css("visibility","hidden")
+    $(".signup-box").slideDown();
+    $(".close").css("visibility","visible")
+    $(".detail-dis").fadeOut()
+})
+
+$(".close").click(function()
+{
+    $(".signup-box").slideUp();
+    $(".signup-rout").css("visibility","visible")
+    // $(".close").css("visibility","hidden")
+    $(".detail-dis").fadeIn()
+})
+
+
+
+
+$(".work-detail").hide();
+
+$(".sw-link").click(function()
+{   $(".sw-link").css("visibility","hidden")
+    $(".work-detail").slideDown();
+    $(".close-sw").css("visibility","visible")
+    $(".about-stopwatch-discrip-para").fadeOut()
+})
+
+$(".close-sw").click(function()
+{
+    $(".work-detail").slideUp();
+    $(".sw-link").css("visibility","visible")
+    // $(".close").css("visibility","hidden")
+    $(".about-stopwatch-discrip-para").fadeIn()
+})
---
+++ server.js
@@ -0,0 +1,9 @@
+const https=require('https')
+const express=require('express')
+app=express();
+app.use(express.static('public'))
+app.get("/", (req,res)=>
+{
+   res.sendFile(__dirname+"/index.html")
+})
+app.listen(3000)
