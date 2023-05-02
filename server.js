e2bc51dd7e52f72214b2076df80c816ddd5a7028
nikhil2060 <102829906+nikhil2060@users.noreply.github.com>
2023-05-02 13:41:06 +0530

    margin

--- server.js
+++ server.js
@@ -1,41 +1,35 @@
-const https=require('https')
-const express=require('express')
-const bodyParser=require('body-parser')
-var name=''
-var goal=''
-var task=''
+const https = require("https");
+const express = require("express");
+const bodyParser = require("body-parser");
+var name = "";
+var goal = "";
+var task = "";
 
 const useMiddleware = (req, resp, next) => {
-   if (goal=="") {
-     console.log("pls")
-   }
-   else {
-      console.log("all filled")
-     next()
-   }
-}
-app=express();
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}));
+  if (goal == "") {
+    console.log("pls");
+  } else {
+    console.log("all filled");
+    next();
+  }
+};
+app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'))
-app.get("/", (req,res)=>
-{
-   res.sendFile(__dirname+"/index.html")
-})
-app.get("/stopwatch",(req,res)=>
-{
-   
-   res.render("stopwatch.hbs",{userName:name})
-})
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/stopwatch", (req, res) => {
+  res.render("stopwatch", { userName: name });
+});
 
-app.post("/",function(req,res)
-{
-   name=(req.body.user)
-   goal=(req.body.goal)
-   task=(req.body.task)
-   res.sendFile(__dirname+'/index.html')
-})
+app.post("/", function (req, res) {
+  name = req.body.user;
+  goal = req.body.goal;
+  task = req.body.task;
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-app.listen(3000)
+app.listen(3000);
