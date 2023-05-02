7d7380ecbbc32e0e117f32800f1e4a6aeeaf86fb
yatharthw <121886758+SaxenaVaibhav80@users.noreply.github.com>
2023-05-02 13:11:34 +0530

    changeee

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const https=require('https')
 const express=require('express')
 const bodyParser=require('body-parser')
-app=express();
+var name=''
+var goal=''
+var task=''
 
+const useMiddleware = (req, resp, next) => {
+   if (goal=="") {
+     console.log("pls")
+   }
+   else {
+      console.log("all filled")
+     next()
+   }
+}
+app=express();
+app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static('public'))
@@ -10,13 +23,17 @@ app.get("/", (req,res)=>
 {
    res.sendFile(__dirname+"/index.html")
 })
-app.get("/stopwatch", (req,res)=>
+app.get("/stopwatch", useMiddleware,(req,res)=>
 {
-   res.sendFile(__dirname+"/stopwatch.html")
+   
+   res.render("stopwatch.hbs",{userName:name})
 })
 
 app.post("/",function(req,res)
 {
+   name=(req.body.user)
+   goal=(req.body.goal)
+   task=(req.body.task)
    res.sendFile(__dirname+'/index.html')
 })
 
