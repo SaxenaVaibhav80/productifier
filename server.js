ea7cab765f745755ffcf38e016e7ea60cc5adc89
yatharthw <121886758+SaxenaVaibhav80@users.noreply.github.com>
2023-05-03 12:27:59 +0530

    vv

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const https = require("https");
 const express = require("express");
 const bodyParser = require("body-parser");
-var name = "";
-var goal = "";
-var task = "";
-
-const useMiddleware = (req, resp, next) => {
-  if (goal == "") {
-    console.log("pls");
-  } else {
-    console.log("all filled");
-    next();
-  }
-};
+var name=''
 app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,9 +10,6 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/stopwatch", (req, res) => {
-  res.render("stopwatch.hbs", { userName: name });
-});
 
 app.post("/", function (req, res) {
   name = req.body.user;
@@ -31,6 +17,17 @@ app.post("/", function (req, res) {
   task = req.body.task;
   res.sendFile(__dirname + "/index.html");
 });
+app.post("/stopwatch", function (req, res) {
+  goal = req.body.goal;
+  task = req.body.task;
+  console.log(goal)
+  console.log(task)
+  res.render("stopwatch.hbs", { userName: name });
+});
+
+// app.get("/stopwatch",(req, res) => {
+//   res.render("stopwatch.hbs", { userName: name });
+// });
 
 app.listen(3000);
 
